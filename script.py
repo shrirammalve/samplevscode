092600e7afb92b717a0856f5767e536a06debbbf
shriram malve <shriram.malve@gmail.com>
2019-07-02 14:15:31 +0530

    deleted

--- script.py
+++ script.py
@@ -6,12 +6,12 @@ print(sys.executable)
 
 
 def greet(who_to_greet):
-    test = "te"
     greeting = "hello,{}".format(who_to_greet)
     return greeting
 
 
-""" print(greet("world"))
-print(greet("shriram")) """
-""" print(requests.get("https://slproweb.com/products/Win32OpenSSL.html").status_code)
-print(r.ok) """
+print(greet("world"))
+print(greet("shriram"))
+print(requests.get("https://slproweb.com/products/Win32OpenSSL.html").status_code)
+print(r.ok)
+
