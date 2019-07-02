9ff9028065d08d155d98535eb712bd5d25c49ed5
shriram malve <shriram.malve@gmail.com>
2019-07-02 14:04:22 +0530

    initial Commit

---
+++ script.py
@@ -0,0 +1,19 @@
+import math
+import sys
+
+import requests
+
+
+print(sys.version)
+print(sys.executable)
+
+
+def greet(who_to_greet):
+    test = "te"
+    greeting = "hello,{}".format(who_to_greet)
+    return greeting
+
+
+""" print(greet("world"))
+print(greet("shriram")) """
+print(requests.get("https://slproweb.com/products/Win32OpenSSL.html").status_code)
